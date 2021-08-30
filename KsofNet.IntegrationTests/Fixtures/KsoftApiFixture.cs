34a10717851ebb32ee6b326fd276b88c39b001e3
TazmamzaT <tazmamzat@gmail.com>
2021-08-30 17:17:45 -0500

    Add start of configuration loading for KsoftApiFixture

--- KsofNet.IntegrationTests/Fixtures/KsoftApiFixture.cs
+++ KsofNet.IntegrationTests/Fixtures/KsoftApiFixture.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KsofNet.IntegrationTests.Fixtures
 {
-    public class KsoftApiFixture
+    public class KsoftApiFixture : IDisposable
     {
+        public IConfiguration Configuration { get; }
+
+        public KsoftApiFixture()
+        {
+            Configuration = new ConfigurationBuilder().AddJsonFile("secrets.json").Build();
+        }
+
+        public void Dispose()
+        {
+            
+        }
     }
 }
