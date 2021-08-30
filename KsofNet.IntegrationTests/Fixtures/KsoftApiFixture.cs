05c1465d74b291b24c519e797c652fa2120e03fd
TazmamzaT <tazmamzat@gmail.com>
2021-08-30 16:32:11 -0500

    Add empty KsoftApiFixture class

---
+++ KsofNet.IntegrationTests/Fixtures/KsoftApiFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KsofNet.IntegrationTests.Fixtures
+{
+    public class KsoftApiFixture
+    {
+    }
+}
