5ed5c13e07ef9d1f131f89496d2190685401f181
TazmamzaT <tazmamzat@gmail.com>
2021-08-30 17:24:43 -0500

    Add RandomNsfw integration test class

---
+++ KsofNet.IntegrationTests/Ksoft/ImagesAPI/RandomNsfw.cs
@@ -0,0 +1,20 @@
+﻿using KsofNet.IntegrationTests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KsofNet.IntegrationTests.Ksoft.ImagesAPI
+{
+    public class RandomNsfw : IClassFixture<KsoftApiFixture>
+    {
+        KsoftApiFixture fixture;
+
+        public RandomNsfw(KsoftApiFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+    }
+}
