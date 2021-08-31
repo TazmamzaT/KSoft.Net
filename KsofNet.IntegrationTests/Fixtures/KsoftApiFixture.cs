7db9aeddf97c919d74992db4c6680d9dd5d6c163
TazmamzaT <tazmamzat@gmail.com>
2021-08-30 19:07:37 -0500

    Implement KSoftApiFixture

--- KsofNet.IntegrationTests/Fixtures/KsoftApiFixture.cs
+++ KsofNet.IntegrationTests/Fixtures/KsoftApiFixture.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KSoftNet;
+using KSoftNet.IntegrationTests.Options;
 
 namespace KSoftNet.IntegrationTests.Fixtures
 {
@@ -11,9 +13,16 @@ namespace KSoftNet.IntegrationTests.Fixtures
     {
         public IConfiguration Configuration { get; }
 
+        public KSoftAPI KSoftAPI { get; }
+
         public KSoftApiFixture()
         {
             Configuration = new ConfigurationBuilder().AddJsonFile("secrets.json").Build();
+
+            KSoftApiOptions ksoftoptions = new KSoftApiOptions();
+            Configuration.GetSection(KSoftApiOptions.KSoftApi).Bind(ksoftoptions);
+
+            KSoftAPI = new KSoftAPI(ksoftoptions.ApiToken);
         }
 
         public void Dispose()
